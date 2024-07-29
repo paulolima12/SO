19ad968eb5ef5abcc88beff04d071a2bbb171bdb
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-07-29 08:15:07 -0300

    first commit

---
+++ processa.php
@@ -0,0 +1,16 @@
+<?php
+
+    $nome = $_POST['nome'];
+    $email =$_POST['email'];
+    $idade =$_POST['idade'];
+
+    echo $nome;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $idade;
+
+
+
+
+?>
